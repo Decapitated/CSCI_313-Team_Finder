c1daf3a60b9b5b061f470417e9d0e21e01c23a34
Brett Johnson <brettjohnsondev@gmail.com>
2020-05-04 00:36:46 -0500

    Virtual Enviroment that works with backend python

---
+++ TeamFinder_Backend_VirtEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\brett\Documents\School Documents\Spring 2020\CSCI 313\Team Project\CSCI_313-Team_Finder\TeamFinder_Backend_VirtEnv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
