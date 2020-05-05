a7896eb6a84df517fd422934f642e467252d0303
Brett Johnson <brettjohnsondev@gmail.com>
2020-05-04 22:41:41 -0500

    Got a lot of the navbar stylings done

---
+++ TeamFinder/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Party } from '../party';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  party: Party = {
+    id: 0,
+    owner: 'headlessdev',
+    created: new Date(),
+    title: 'Leviathan Raid',
+    description: 'I need a full fireteam.',
+    game: 'Destiny 2',
+    maxPlayers: 6, reservedPlayers: 2,
+    members: []
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ TeamFinder/src/app/nav-bar/party-viewer/party-viewer.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Party } from '../../party';
+
+@Component({
+  selector: 'app-party-viewer',
+  templateUrl: './party-viewer.component.html',
+  styleUrls: ['./party-viewer.component.css']
+})
+export class PartyViewerComponent implements OnInit {
+
+  @Input() party: Party;
+  /*party: Party = {
+    id: 0,
+    owner: 'headlessdev',
+    created: new Date(),
+    title: 'Leviathan Raid',
+    description: 'I need a full fireteam.',
+    game: 'Destiny 2',
+    maxPlayers: 6, reservedPlayers: 2,
+    members: []
+  };*/
+  viewParty = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  openViewer(){
+    this.viewParty = !this.viewParty;
+  }
+
+  getNumPlayers(): number {
+    return 1 + this.party.reservedPlayers + this.party.members.length;
+  }
+}
